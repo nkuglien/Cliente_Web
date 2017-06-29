28c9c26f82395ed754042e0fb56be6ec0e3a0d3a
Ezequiel Maranga <ezequiel.maranga@gmail.com>
2017-06-28 21:43:33 -0300

    algo de ui

---
+++ src/servlets/ViewAltaPedidoServlet.java
@@ -0,0 +1,26 @@
+package servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import DTO.PedidoClienteDTO;
+import businessDelegate.PrendaDelegate;
+
+@WebServlet("/pedidos/alta/view")
+public class ViewAltaPedidoServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		req.setAttribute("prendas", PrendaDelegate.GetInstancia().getAllPrendas());
+		req.setAttribute("nuevoPedido", new PedidoClienteDTO());
+		req.getRequestDispatcher("/jsp/viewAltaPedido.jsp").forward(req, resp);
+	}
+
+}
